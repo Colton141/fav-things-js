62a2dbbf34e400d0afa0ac46c0aa076123481237
Colton Lacey and heather zurek <colton@>
2019-04-01 11:50:18 -0700

    make new array, list new array

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,28 @@
 $(document).ready(function(){
   $("form#favorites").submit(function(event){
-  var foodInput = $("Input#food").val();
+  event.preventDefault();
+  var foodInput = $("input#food").val();
   var movieInput = $("input#movie").val();
   var icecreamInput = $("input#icecream").val();
   var candyInput = $("input#candy").val();
   var animalInput = $("input#animal").val();
   var favorite = [foodInput, movieInput, icecreamInput, candyInput, animalInput];
+  var newArray = ["bug", "vegetable"];
 
-    $('.result').append(favorite[1 + 0 + 2]);
+  // $('.result').append(favorite[2], favorite[0]);
 
-    event.preventDefault();
+
+  // var newArray = [newArray.push(newFav)];
+  newArray.push(favorite[1], favorite[0], favorite[2]);
+  $('.result').append("<li>",newArray[0],"</li>");
+  $('.result').append("<li>",newArray[1],"</li>");
+  $('.result').append("<li>",newArray[2],"</li>");
+  $('.result').append("<li>",newArray[3],"</li>");
+  $('.result').append("<li>",newArray[4],"</li>");
+
+
+
+
 
   });
 });
